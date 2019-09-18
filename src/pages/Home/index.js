e4ae6404f55f5e25f104c94567e739b345cf4a72
Andre Work <andremregodev@gmail.com>
2019-09-18 15:13:52 -0300

    Estilizando página Home e adicionando o polished

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,90 @@
 import React from 'react'
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-cano-alto-triton-masculino/06/E15-0763-006/E15-0763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Legal</strong>
+        <span>R$ 129</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-cano-alto-triton-masculino/06/E15-0763-006/E15-0763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Legal</strong>
+        <span>R$ 129</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-cano-alto-triton-masculino/06/E15-0763-006/E15-0763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Legal</strong>
+        <span>R$ 129</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-cano-alto-triton-masculino/06/E15-0763-006/E15-0763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Legal</strong>
+        <span>R$ 129</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-cano-alto-triton-masculino/06/E15-0763-006/E15-0763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Legal</strong>
+        <span>R$ 129</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
